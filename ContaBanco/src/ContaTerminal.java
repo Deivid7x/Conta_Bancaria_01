0397ac838f338a33e182aac1b455f68e6c3ad887
Deivid7x <115668940+Deivid7x@users.noreply.github.com>
2023-08-30 13:30:46 -0300

    Criado o projeto ContaBanco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite o numero da sua conta: ");
+        int numeroDaConta = scanner.nextInt();
+
+        System.out.println("Digite sua agencia: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome e sobrenome: ");
+        String nomeDoUsuario = scanner.next();
+        String sobrenomeDoUsuario = scanner.nextLine();
+
+        System.out.println("Digite seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        System.out.println("Olá " + nomeDoUsuario + sobrenomeDoUsuario + ", obrigado por criar uma conta em nosso banco, sua agência é: " + agencia + ", conta: " + numeroDaConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+    }
+}
